a6892bd7e632d024f9b61518a681dcdb73728323
tripledoublev <tripledoublev@users.noreply.github.com>
2025-02-18 22:40:19 +0000

    deploy: e214da45bee0e23601efd8762557e6952eb16256

--- assets/nav.js
+++ assets/nav.js
@@ -2,10 +2,10 @@ class CustomNav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="container">
-            <a href="https://hypha.coop" class="title-link">
+            <a href="/" class="title-link">
                 <h1 class="title">
                 <span class="mr2 purple uppercase">Hypha</span>
-                <span class="gray regular">Worker Co-operative</span>
+                <span class="gray regular">Testnets</span>
                 </h1>
             </a>
              <nav>
@@ -16,6 +16,7 @@ class CustomNav extends HTMLElement {
                     
                         <li><a href="/">Home</a></li>
                         <li><a href="/about-the-program/">About the program</a></li>
+                        <li><a target="_blank" href="https://hypha.coop">About Hypha <img class="nav-link-img" src="./assets/image/overlap-character.png" alt="Overlap Unicode Character (🗗/U+1F5D7)"></a></li>
                     
 
                     <!-- Page links in navigation are inserted as li elements above this line --!>
